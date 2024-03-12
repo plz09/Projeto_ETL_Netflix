6dde26274f88f09c5ed6cea934adb6eafd93e235
Ravi Pellizzi <r.pellizzi09@gmail.com>
2024-03-12 09:13:20 -0300

    Preparando leitura dos arquivos

--- src/scripts/main.py
+++ src/scripts/main.py
@@ -1,1 +1,21 @@
-import pandas as pd;
+import pandas as pd
+import os
+import glob
+
+folder_path = 'src\\data\\raw'
+
+excel_files = glob.glob(os.path.join(folder_path, '*.xlsx'))
+
+if not excel_files:
+    print('Nenhum arquivo compatível encontrado.')
+else:
+
+    dfs = []
+
+    for excel_file in excel_files:
+
+        try:
+            df_temp = pd.read_excel(excel_file)
+            print(df_temp)
+        except Exception as e:
+            print(f'Erro ao ler o arquivo. {excel_file} : {e}')
