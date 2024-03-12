cf80973c24cb176e767501bc795c3565c448e0d1
Ravi Pellizzi <r.pellizzi09@gmail.com>
2024-03-12 10:58:03 -0300

    Tratando arquivos, criando tabelas, preparando e salvando arquivo de saida

--- src/scripts/main.py
+++ src/scripts/main.py
@@ -16,6 +16,43 @@ else:
 
         try:
             df_temp = pd.read_excel(excel_file)
-            print(df_temp)
+
+            # Cria coluna location consoante ao nome do arquivo
+
+            file_name = os.path.basename(excel_file)
+            if 'brasil' in file_name:
+                df_temp['location'] = 'br'
+            elif 'france' in file_name:
+                df_temp['location'] = 'fr'
+            elif 'italian' in file_name:
+                df_temp['location'] = 'it'
+
+            # Cria coluna 'campaign' 
+                
+            df_temp['campaign'] = df_temp['utm_link'].str.extract(r'utm_campaign=(.*)')
+            dfs.append(df_temp)
+            print(dfs)
+
         except Exception as e:
             print(f'Erro ao ler o arquivo. {excel_file} : {e}')
+
+if dfs:
+
+    # Concatena as tabelas para formar uma tabela só
+    result = pd.concat(dfs, ignore_index=True)
+
+    # Caminho de saída
+    output_file = os.path.join('src', 'data', 'data_ready', 'clean.xlsx')
+
+    # Configura motor de escrita
+    writer = pd.ExcelWriter(output_file, engine='xlsxwriter')
+
+    # Leva os dados do result para serem escritos no motor do excel configurado
+    result.to_excel(writer, index=False)
+    # Salva o arquivo de excel
+    writer._save()
+else:
+    print('Nenhum dado para salvar.')
+
+
+
